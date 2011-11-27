f0991e566916de0842c65f81d261cd0d814e81e2
wjzz <wjedynak@gmail.com>
2011-11-27 02:48:42 +0100

    Started notes for mercurial.

---
+++ Mercurial/notes.sh
@@ -0,0 +1,21 @@
+# mercurial cheatsheet
+# based on http://hgbook.red-bean.com/
+
+## Chapter 02
+
+# version check
+hg version
+
+# help
+hg help
+hg help -v
+hg help init
+
+# init
+hg init repo-name
+
+# clone
+hg clone repo-url
+
+# log
+hg log
