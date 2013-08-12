3bc4aaf03b6725f57c20cb442a2e3efa81eeee1a
Wojciech Jedynak <wjedynak@gmail.com>
2013-08-12 17:24:23 +0200

    Update new_tricks.sh

--- Linux/new_tricks.sh
+++ Linux/new_tricks.sh
@@ -9,3 +9,15 @@ zcat out.gz >in
 zcat out.zip >in2
 unzip -p out.zip
 
+# `grep`ing
+grep -f patterns input  #select lines matching any of the given patterns
+grep -v pattern input  # inverts the selection
+grep -c pattern input # count the lines, suppress output
+egrep # uses regular expressions in patterns
+
+# hosts
+cat /etc/hosts
+
+# full system info
+uname -a
+cat /etc/issue   # system name
